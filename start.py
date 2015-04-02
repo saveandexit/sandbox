ca384bb9c7061f95fecaf04423f3e73a5fba57f7
saveandexit <oh.hai.mike@gmail.com>
2015-04-02 16:29:36 +0300

    Update start.py

--- start.py
+++ start.py
@@ -1,4 +1,7 @@
-intlist  = []
-for num in range(0,10):  #1. Создание списка целых
-  intlist.append(num)
+intlist = []
+import random
+while len(intlist)<9:
+  num = random.randint(1,10)
+  if num not in intlist:
+    intlist.append(num)              # 1. Создание списка целых чисел
 
