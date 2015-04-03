fdec87b468e0ef22af761990810cd085af970d20
Mike Abramychev <oh.hai.mike@gmail.com>
2015-04-03 16:52:36 +0300

    Update start.py

--- start.py
+++ start.py
@@ -2,15 +2,20 @@ intlist = []
 def fill(hhg):
 	import random
 	while len(hhg)<9:
-		num = random.randint(0,9)
+		num = random.randint(0,9)  #1. Получение списка случайных целых
 		if num not in hhg:
 			hhg.append(num)
 	return hhg
 fill(intlist)
 
-n = 1
-while len(intlist)>n:
-	for i in range(len(intlist)-n):
-		if intlist[i]>intlist[i+1]:
-			intlist[i],intlist[i+1]=intlist[i+1],intlist[i]   #2. Сортировка методом пузырька
-	n+=1
+def swap(bfg,a,b):
+	bfg[a],bfg[b]=bfg[b],bfg[a]
+def bubble_swap(bfg):
+	a = len(bfg)
+	while a>1:
+		for b in range(a-1):
+			if bfg[b]>bfg[b+1]:
+				swap(bfg,b,b+1)    #2. Сортировка методом пузырька
+		a -= 1
+
+bubble_swap(intlist)
