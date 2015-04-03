3f05386c3d6b4fddf224217760e9629874ee1b61
saveandexit <oh.hai.mike@gmail.com>
2015-04-03 15:09:48 +0300

    Update start.py

--- start.py
+++ start.py
@@ -5,3 +5,9 @@ while len(intlist)<9:
   if num not in intlist:
     intlist.append(num)              # 1. Создание списка целых чисел
 
+n = 1
+while len(intlist)>n:
+	for i in range(len(intlist)-n):
+		if intlist[i]>intlist[i+1]:
+			intlist[i],intlist[i+1]=intlist[i+1],intlist[i]   #2. Сортировка методом пузырька
+	n+=1
